8b4c15a5609bc26e8b1bf11c182d18462796cfef
Дурусалиев Алмаз Муратович <adurusaliev@megacom.local>
2023-05-19 13:00:18 +0600

    fix: uncommited functions for sending message

--- distribution/utils.py
+++ distribution/utils.py
@@ -7,16 +7,6 @@ from django.conf import settings
 from message.models import Message
 
 
-# def send_sms(distribution_model, user):
-#     client = Client(settings.TWILIO_SID, settings.TWILIO_AUTH_TOKEN, http_client=settings.TWILIO_HTTP_PROXY)
-#     sms = client.messages.create(
-#             body=distribution_model.get("text"),
-#             from_=settings.TWILIO_PHONE_NUMBER,
-#             to='+' + str(user.phone_number)
-#         )
-#     return sms
-
-
 def send_sms(distribution_model, user, message):
     api = 'https://probe.fbrq.cloud/v1/send'
 
--- distribution/views.py
+++ distribution/views.py
@@ -26,23 +26,23 @@ class DistributionDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Distribute.objects.all()
     serializer_class = DistributeSerializer
 
-    # def perform_update(self, serializer):
-    #     self.update(serializer)
-    #
-    #     instance_id = serializer.data.get('id')
-    #     instance = Distribute.objects.get(id=instance_id)
-    #     task_id = instance.task_id
-    #     task = send_message.AsyncResult(task_id)
-    #     task.eta = serializer.data.get("sending_datetime")
-    #     task.save()
-    #     return instance
-    #
-    # def partial_update(self, request, *args, **kwargs):
-    #     self.partial_update(request, *args, **kwargs)
-    #
-    #     instance = self.get_object()
-    #     task_id = instance.task_id
-    #     task = send_message.AsyncResult(task_id)
-    #     task.eta = instance.sending_datetime
-    #     task.save()
-    #     return instance
+    def perform_update(self, serializer):
+        self.update(serializer)
+
+        instance_id = serializer.data.get('id')
+        instance = Distribute.objects.get(id=instance_id)
+        task_id = instance.task_id
+        task = send_message.AsyncResult(task_id)
+        task.eta = serializer.data.get("sending_datetime")
+        task.save()
+        return instance
+
+    def partial_update(self, request, *args, **kwargs):
+        self.partial_update(request, *args, **kwargs)
+
+        instance = self.get_object()
+        task_id = instance.task_id
+        task = send_message.AsyncResult(task_id)
+        task.eta = instance.sending_datetime
+        task.save()
+        return instance
